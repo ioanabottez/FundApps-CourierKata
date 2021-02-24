5fcdda8e258a2c5000c4adc02eb2f5b75590da40
ioanabottez <ibottez@yahoo.com>
2021-02-24 21:47:53 +0000

    Implement speedy shipping and test

--- src/costCalculator.js
+++ src/costCalculator.js
@@ -1,29 +1,44 @@
 const fs = require('fs');
+const readFile = fs.readFileSync('parcel.json');
+const newData = JSON.parse(readFile);
 
-    const getOrderPrice = (order) => {
-        const readFile = fs.readFileSync('parcel.json');
-        const newData = JSON.parse(readFile);
-
+    const getOrderPrice = (order, isSpeedy) => {
+       
         let orderArray = [];
-        order.forEach(parcel => {
+        order.parcels.forEach(parcel => {
             const found = newData.parcel.find(element => parcel.size === element.size);
             const size = parcel.size;
             const quantity = parcel.quantity;
 
-            const receipt = 
-                {
-                    size, 
-                    quantity,
-                    pricePerItem: found.price,
-                    totalPrice: found.price * quantity,
-                };
-            orderArray.push(receipt);
+            const items = 
+            {
+                size, 
+                quantity,
+                pricePerItem: found.price,
+                totalPrice: found.price * quantity,
+            };
+            orderArray.push(items);
         });
 
-        return orderArray;
+        if(isSpeedy)
+        {
+            const receipt = {
+                speedyShipping: orderArray.reduce((a,b) => a + (b['totalPrice'] || 0), 0),
+                orderTotal: orderArray.reduce((a,b) => a + (b['totalPrice'] || 0), 0) * 2,
+                parcels: orderArray,
+            };
+
+            return receipt;
+        }
+       else
+            {
+                const receipt = {
+                    parcels: orderArray,
+                };
+                return receipt;
+            }
 
     };
-
 
 module.exports = {
     getOrderPrice
--- test/costCalculator.test.js
+++ test/costCalculator.test.js
@@ -1,29 +1,29 @@
 const costCalculator = require('../src/costCalculator');
 
 describe('getOrderPrice', function() {
-    it("should calculate the correct price for a basic order", function(){
-        const order = [
+        const order ={ 
+            parcels: [
             {
                 size: 'Small',
-                quantity: 1,
+                quantity: 1
             },
             {
                 size: 'Medium',
-                quantity: 1,
+                quantity: 1
             },
             {
                 size: 'Large',
-                quantity: 2,
+                quantity: 2
             },
             {
                 size: 'XL',
                 quantity: 3
-            },
+            }
+        ],
+    };
 
-
-        ];
-
-        const receipt = [
+        const receipt = {
+            parcels:[
             {
                 size: 'Small',
                 quantity: 1,
@@ -48,7 +48,44 @@ describe('getOrderPrice', function() {
                 pricePerItem: 25,
                 totalPrice: 75,
             }
-        ];
-        expect(costCalculator.getOrderPrice(order)).toEqual(receipt);
+        ],
+    };    
+
+        const receipt2 = {
+            speedyShipping: 116,
+            orderTotal: 232,
+            parcels:[
+            {
+                size: 'Small',
+                quantity: 1,
+                pricePerItem: 3,
+                totalPrice: 3,
+            },
+            {
+                size: 'Medium',
+                quantity: 1,
+                pricePerItem: 8,
+                totalPrice: 8,
+            },
+            {
+                size: 'Large',
+                quantity: 2,
+                pricePerItem: 15,
+                totalPrice: 30,
+            },
+            {
+                size: 'XL',
+                quantity: 3,
+                pricePerItem: 25,
+                totalPrice: 75,
+            }
+        ],
+    }
+    it("should calculate the correct price for a basic order without speedy delivery", function(){
+        expect(costCalculator.getOrderPrice(order, false)).toEqual(receipt);
+    });
+
+    it("should calculate the correct price for a basic order with speedy delivery", function(){
+        expect(costCalculator.getOrderPrice(order, true)).toEqual(receipt2);
     });
 }); 
